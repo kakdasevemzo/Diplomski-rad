1a130a044c0d35147d9e4701f1e36e6402d7943b
kakdasevemzo <matija.radovic@protonmail.com>
2024-08-16 13:34:16 +0200

    fixes

--- API/api/urls.py
+++ API/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import BlogPostListCreate, upload_telemetry
+from .views import upload_telemetry
 
 
 urlpatterns = [
-    path("blogposts/", BlogPostListCreate.as_view(), name="blogpost-view-create"),
     path('sondes/telemetry/', upload_telemetry, name='upload_telemetry'),
 ]
